c0d1d04ee4017c325e332080af98b0fa65f1d84a
Incloud3 <dominoday222@gmail.com>
2023-09-27 09:43:04 +0200

    Moved to Testing branch

--- TaskManagementSystem/Models/TaskPlanned.cs
+++ TaskManagementSystem/Models/TaskPlanned.cs
@@ -1,12 +1,18 @@
+using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Identity;
 namespace TaskManagementSystem.Models;
 
 public class TaskPlanned
 {
+    [Required]
     public int Id { get; set; }
+    [Required]
     public string Title { get; set; }
+    [Required]
     public string Description { get; set; }
+    [Required]
     public DateTime Deadline { get; set; }
+    [Required]
     public string Priority { get; set; }
 
     public string UserId { get; set; }
