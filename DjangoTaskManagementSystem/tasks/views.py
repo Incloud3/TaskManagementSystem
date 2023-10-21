7798dd5169c9a03b151211f930192d142f212a4e
Incloud3 <dominoday222@gmail.com>
2023-10-21 17:30:02 +0200

    Fixed creation/edit/delete operations. Working on task_list

--- DjangoTaskManagementSystem/tasks/views.py
+++ DjangoTaskManagementSystem/tasks/views.py
@@ -94,17 +94,17 @@ def delete_task(request, task_id):
     task = get_object_or_404(TaskPlanned, id=task_id)
     if request.method == "POST":
         task.delete()
-        return redirect('tasks')  # Assuming 'tasks' is the name of the URL pattern that lists all tasks
+        return redirect('task_list')
     return render(request, 'delete_task_confirm.html', {'task': task})
 
 def edit_task(request, task_id):
     task = get_object_or_404(TaskPlanned, id=task_id)
     if request.method == "POST":
-        # Assuming you have a TaskForm to handle task data
+
         form = TaskForm(request.POST, instance=task)
         if form.is_valid():
             form.save()
-            return redirect('tasks')  # Again, assuming 'tasks' is the name of the URL pattern that lists all tasks
+            return redirect('task_list')
     else:
         form = TaskForm(instance=task)
     return render(request, 'edit_task.html', {'form': form, 'task': task})
