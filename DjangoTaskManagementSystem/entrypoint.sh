b2d42a32ce1c11e455371002dbf55bb660b966a1
Incloud3 <Incloud3@gmail.com>
2023-12-02 18:31:25 +0100

    Added automatic migrations to dockerfile after 30 seconds from starting container

--- DjangoTaskManagementSystem/entrypoint.sh
+++ DjangoTaskManagementSystem/entrypoint.sh
@@ -1,7 +1,12 @@
-#!/bin/sh
+#!/bin/bash
+
+# Basic wait before starting the application
+echo "Waiting for 30 seconds..."
+sleep 30
 
 # Apply database migrations
 echo "Applying database migrations..."
 python manage.py migrate
 
+# Start the application
 exec "$@"
